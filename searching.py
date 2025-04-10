8a0b5474358a1680cd6c87660e8a820110b20259
Jakub Homola <j.homola@post.cz>
2025-04-10 08:56:43 +0200

    more

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,10 +13,18 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_name, "r") as data:
+        reader = json.load(data)
+        if field in reader:
+            sequencial_data = reader[field]
+            return sequencial_data
+        else:
+            return "None"
+
 
 
 def main():
-    pass
+    print(read_data("sequential.json", "unordered_numbers"))
 
 
 if __name__ == '__main__':
